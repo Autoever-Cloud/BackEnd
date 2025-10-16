b522131e8d5102bf35e02923221f180628691e11
HS <lhs06185@gmail.com>
2025-10-16 17:50:00 +0900

    EC2 change

--- src/main/java/com/example/demo/handler/sologController.java
+++ src/main/java/com/example/demo/handler/sologController.java
@@ -32,7 +32,6 @@ public class sologController {
 
 			return Map.of("response", responseLLM);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return Map.of("status", "error");
 		}
